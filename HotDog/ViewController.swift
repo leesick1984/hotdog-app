f470aae8a8d67815d65f9426adb562111de1fab5
Alexander Lee <genius.lees@gmail.com>
2021-12-29 22:11:13 +0600

    Initial Commit

---
+++ HotDog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotDog
+//
+//  Created by Alexander Lee on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
